c911ceb208699ce3dc41fe957f48237fe5085a8a
Abdumuxtor <abdumuxtorabdullajonov9@gmail.com>
2026-09-21 21:22:33 +0500

    yangilandi

--- core/settings.py
+++ core/settings.py
@@ -166,3 +166,19 @@ CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {"handlers": ["console"], "level": "INFO"},
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
